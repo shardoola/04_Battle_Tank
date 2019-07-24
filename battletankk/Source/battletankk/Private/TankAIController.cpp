8c1daf14ad15337de964ebee43ce1691ccc22cb9
shardoolask <shardoolask123@gmail.com>
2019-07-24 21:33:56 +0530

    AItank can find your tank

---
+++ battletankk/Source/battletankk/Private/TankAIController.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "TankAIController.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "Components/ActorComponent.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIcontroller can't find your tank"));
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIcontroller  can find your tank: %s"), *PlayerTank->GetName());
+	}
+	UE_LOG(LogTemp, Warning, TEXT("AIcontroller begin play"));
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const 
+{
+	
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) {
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
+
+}
