6b9c9b2a8c6857a96b65d7232fa71d31879f2ed7
shardoolask <shardoolask123@gmail.com>
2019-08-06 23:19:18 +0530

    tsnk move

--- battletankk/Source/battletankk/Private/Projectile.cpp
+++ battletankk/Source/battletankk/Private/Projectile.cpp
@@ -30,8 +30,8 @@ void AProjectile::Tick(float DeltaTime)
 }
 void AProjectile::LaunchProjectile(float speed) {
 
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f proejetle fires at %f"), Time,speed);
+	
+	
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * speed);
 	ProjectileMovement->Activate();
 
---
+++ battletankk/Source/battletankk/Public/TankTrack.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * Tanktrack is used to set max drivin force,and to apple forces to the tank
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANKK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable,Category=Input)
+	void SetThrottle(float Throttle);
+	//max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000000;//assume 40 ton tank,1 g accleration
+};
